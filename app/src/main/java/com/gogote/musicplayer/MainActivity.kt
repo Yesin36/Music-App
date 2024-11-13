f8972a6003524553a078c41e8fde0d3d086bb68b
Nishat <shourovhossainnishat@gmail>
2024-11-13 20:06:02 +0600

    Navigation Drawer

--- app/src/main/java/com/gogote/musicplayer/MainActivity.kt
+++ app/src/main/java/com/gogote/musicplayer/MainActivity.kt
@@ -3,16 +3,21 @@ package com.gogote.musicplayer
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.gogote.musicplayer.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,6 +30,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // for nev drawer
+        toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         binding.shufflebtn.setOnClickListener {
             val intent = Intent(this, PlayerActivity::class.java)
             startActivity(intent)
@@ -37,6 +49,20 @@ class MainActivity : AppCompatActivity() {
         binding.playlistbtn.setOnClickListener{
             val intent = Intent(this, PlaylistActivity::class.java)
             startActivity(intent)
+        }
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navFeedback -> {Toast.makeText(baseContext, "Feedback", Toast.LENGTH_SHORT).show()
+                    true}
+                R.id.navSettings -> {Toast.makeText(baseContext, "Setting", Toast.LENGTH_SHORT).show()
+                true}
+                R.id.navAbout -> {Toast.makeText(baseContext, "About", Toast.LENGTH_SHORT).show()
+                    true}
+                R.id.navExit -> exitProcess(1)
+
+                else -> false
+
+            }
         }
 
     }
@@ -77,5 +103,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            return true
+
+        return super.onOptionsItemSelected(item)
+    }
 
 }
